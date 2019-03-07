aba2be6be16239e3487fd8da28a35f81af192bc2
5e5e <studenthwan@gmail.com>
2019-03-08 00:26:58 +0900

    codility lesson 3 TapeEquilibrium 100%

--- algorithm/test/codility/TapeEquilibriumTest.java
+++ algorithm/test/codility/TapeEquilibriumTest.java
@@ -18,79 +18,40 @@ public class TapeEquilibriumTest {
 
 	@Test
 	public void testCase1() {
-		int[] A = new int[]{3, 1, 2, 5, 4};
-		quickSort(A, 0, 4);
-		for (int i : A) {
-			logger.debug(i + "");
-		}
+		assertEquals(1, tapeEquilibrium.solution(new int[]{3, 1, 2, 4, 3}));
+	}
+	@Test
+	public void testCase2() {
+		assertEquals(1, tapeEquilibrium.solution(new int[]{3, 1, 2, 4, 3}));
+	}
+	@Test
+	public void testCase3() {
+		assertEquals(1, tapeEquilibrium.solution(new int[]{3, 1, 2, 4, 3}));
+	}
+	@Test
+	public void testCase4() {
 		assertEquals(1, tapeEquilibrium.solution(new int[]{3, 1, 2, 4, 3}));
 	}
 
-	private void quickSort(int[] A, int low, int high) {
-		if (low < high) {
-			int p = partition(A, low, high);
-			logger.debug("p : " + p);
-			quickSort(A, low, p);
-			quickSort(A, p + 1, high);
-		}
-	}
-
-	private int partition(int[] a, int low, int high) {
-		int pivot = a[(low + high) / 2];
-		int i = low - 1;
-		int j = high + 1;
-		while (true) {
-			logger.debug("pivot : " + pivot);
-			i += 1;
-			while (a[i] < pivot) i += 1;
-			logger.debug("a[i] : " + a[i]);
-			j -= 1;
-			while ( a[j] > pivot) j -= 1;
-			logger.debug("a[j] : " + a[j]);
-			if (i >= j) return j;
-			int temp = a[i];
-			a[i] = a[j];
-			a[j] = temp;
-		}
-	}
 
 	private class TapeEquilibrium {
-		// A = 배열 N = 배열의 길이 P = 0< p<N
 		public int solution(int[] A) {
-			int answer = quickSum(A, 0, A.length-1);
-			int count = 0;
-			int N = A.length;
-			int P = 1;
-			while (P < N) {
-				int leftResult = 0;
-				int rightResult = 0;
-				int finalResult = 0;
-				for (int i = 0; i <= P - 1; i++) {
-					leftResult += A[i];
-				}
-				for (int i = P; i < N; i++) {
-					rightResult += A[i];
-				}
-				finalResult = absolute(leftResult, -rightResult);
-				if (count == 0 || answer >= finalResult) {
-					answer = finalResult;
-				}
-				count += 1;
-				P += 1;
+			int answer = 0;
+			for (int i = 0; i < A.length; i++) {
+				answer += A[i];
 			}
-			return answer;
+			int temp = 0;
+			int bounder = 0;
+
+			int diff = Integer.MAX_VALUE;
+			for (int P = 1; P < A.length; P++) {
+				temp += A[P - 1];
+				bounder = answer - temp;
+				diff = Math.min(diff, Math.abs(bounder - temp));
+			}
+			return diff;
 		}
 
-		private int quickSum(int[] a, int low, int high) {
-			int p = 0;
-			if (low < high) {
-				
-			}
-			return p =;
-		}
 
-		private int absolute(int leftResult, int rightResult) {
-			return (leftResult + rightResult) < 0 ? -(leftResult + rightResult) : (leftResult + rightResult);
-		}
 	}
 }
