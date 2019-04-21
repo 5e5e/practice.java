f12a108f3b1e5fe06b6b40f156ed4d45853d2aa1
5e5e <studenthwan@gmail.com>
2019-04-21 23:01:16 +0900

    practice generic step2 generic interface

--- interface/src/Main.java
+++ interface/src/Main.java
@@ -3,32 +3,82 @@ import org.slf4j.LoggerFactory;
 
 public class Main {
 	private static final Logger logger = LoggerFactory.getLogger(Main.class);
-	static MyHolder myHolder = new MyHolder();
-
-	static void produce() {
-		myHolder.set(6);
-		// ClassCastException
-//		myHolder.set("6");
-	}
-
-	static void consume() {
-		logger.debug((Integer) myHolder.get() / 2 + "");
-	}
 
 	public static void main(String[] args) {
-		produce();
-		consume();
+		IMyHolder<String> myHolder1 = new MyHolder1<String>();
+		IMyHolder<String> myHolder2 = new MyHolder2<String, Integer>();
+		IMyHolder<String> myHolder3 = new MyHolder3();
+		IMyHolder<String> myHolder4 = new MyHolder4<String>();
 	}
 
-	static class MyHolder {
-		Object item;
+	static interface IMyHolder<T> {
+		void set(T item);
 
-		void set(Object obj) {
-			item = obj;
+		T get();
+	}
+
+	static class MyHolder1<T> implements IMyHolder<T> {
+		T item;
+
+		@Override
+		public void set(T item) {
+			this.item = item;
 		}
 
-		Object get() {
+		@Override
+		public T get() {
 			return item;
 		}
 	}
+
+	static class MyHolder2<T, E> implements IMyHolder<T> {
+		T item;
+
+		@Override
+		public void set(T item) {
+			this.item = item;
+		}
+
+		@Override
+		public T get() {
+			return item;
+		}
+
+		public void print(E item) {
+			logger.debug("" + item);
+		}
+	}
+
+	static class MyHolder3 implements IMyHolder<String> {
+		String item;
+
+		@Override
+		public void set(String item) {
+			this.item = item;
+		}
+
+		@Override
+		public String get() {
+			return item;
+		}
+	}
+
+	static class MyHolder4<T> implements IMyHolder<String> {
+		String item;
+
+		@Override
+		public void set(String item) {
+			this.item = item;
+		}
+
+		@Override
+		public String get() {
+			return item;
+		}
+
+		public void print(T item) {
+			logger.debug("" + item);
+		}
+	}
+
 }
